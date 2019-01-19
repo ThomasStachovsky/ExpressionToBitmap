4df632f062b9bc75032d28b238028f9b4971d07a
Tomasz Stachowski <thegreyraveen@gmail.com>
2019-01-19 21:52:34 +0100

    skonczony algebraic to rpn

--- tree.c
+++ tree.c
@@ -11,142 +11,162 @@
 
 node *convertExpressionToTree(char *expression)
 {
-        unit *rpn=convertAlgebraicToRPN(expression);
-        node *root_of_tree=convertRPNToTree(rpn);
+        unit *rpn = convertAlgebraicToRPN(expression);
+        node *root_of_tree = convertRPNToTree(rpn);
         return root_of_tree;
 }
 
-
 node *createNode(char *expression)
 {
-        node *first_node=(node *)malloc(sizeof(node));
-        first_node->left=NULL;
-        first_node->right=NULL;
-        strcpy(first_node->value.expression,expression);
-        first_node->value.type=EXPRESSION;
+        node *first_node = (node *)malloc(sizeof(node));
+        first_node->left = NULL;
+        first_node->right = NULL;
+        strcpy(first_node->value.expression, expression);
+        first_node->value.type = EXPRESSION;
         return first_node;
 }
 
 node *operateOnNode(node *first, node *second, char operator)
 {
-        node *result=malloc(sizeof(node));
-        result->value.type=OPERATOR;
-        result->value.expression[0]=operator;
-        result->value.expression[1]='\0';
-        result->left=first;
-        result->right=second;
+        node *result = malloc(sizeof(node));
+        result->value.type = OPERATOR;
+        result->value.expression[0] = operator;
+        result->value.expression[1] = '\0';
+        result->left = first;
+        result->right = second;
         return result;
 }
 
 bool isOperator(char character)
 {
-        if(character=='(' || character==')' || character=='/'
-           || character=='*' || character=='-' || character =='+'
-           || character=='^' || character=='_')
+        if (character == '(' || character == ')' || character == '/' || character == '*' || character == '-' || character == '+' || character == '^' || character == '_')
                 return true;
-        else return false;
+        else
+                return false;
 }
 
 int weight(char operator)
 {
         //not for ( and )
-        if(operator=='^' || operator=='_')
+        if (operator== '@')
+                return 4;
+        if (operator== '^' || operator== '_')
                 return 3;
-        if(operator=='*' || operator=='/')
+        if (operator== '*' || operator== '/')
                 return 2;
-        if(operator=='+' || operator == '-')
+        if (operator== '+' || operator== '-')
                 return 1;
-        else return 0;
+        if (operator== ',')
+                return 0;
+        else
+                return -1;
+}
+
+int associativity(char operator)
+{
+        if (operator== '^' || operator== '@')
+                return RIGHT_ASSOCIATIVITY;
+        else
+                return LEFT_ASSOCIATIVITY;
 }
 
 unit *convertAlgebraicToRPN(char *expression)
 {
-        unsigned int size_of_expression=strlen(expression);
-        unsigned int expression_iterator=0;
+        unsigned int size_of_expression = strlen(expression);
+        unsigned int expression_iterator = 0;
 
-        unit *rpn=(unit*)malloc(sizeof(unit)*RPN_ARRAY_SIZE);
-        unsigned int rpn_iterator=0;
+        unit *rpn = (unit *)malloc(sizeof(unit) * RPN_ARRAY_SIZE);
+        unsigned int rpn_iterator = 0;
 
         char operator_stack[RPN_ARRAY_SIZE];
-        unsigned int operator_stack_iterator=0;
-
-
+        unsigned int operator_stack_iterator = 0;
 
         char character;
 
-
         char current_expression[STRING_SIZE];
-        current_expression[0]='\0';
+        current_expression[0] = '\0';
 
         do
         {
 
-                character=expression[expression_iterator++];
-                if(isspace(character))
+                character = expression[expression_iterator];
+                if (isspace(character))
                         continue;
 
-                if(isOperator(character))
+                if (isOperator(character))
                 {
-//cos
+                        bool nonempty_previous_expression = false;
 
-                        if(current_expression[0]!='\0')
+                        if (current_expression[0] != '\0')
                         {
-                                strcpy(rpn[rpn_iterator].expression,current_expression);
-                                rpn[rpn_iterator].type=EXPRESSION;
+                                strcpy(rpn[rpn_iterator].expression, current_expression);
+                                rpn[rpn_iterator].type = EXPRESSION;
                                 rpn_iterator++;
-                                current_expression[0]='\0';
+                                current_expression[0] = '\0';
+                                nonempty_previous_expression = true;
                         }
 
-                        if(character=='(')
+                        if (character == '(')
                         {
-                                operator_stack[operator_stack_iterator++]=character;
-                                continue;
+                                if (expression_iterator != 0 && nonempty_previous_expression)
+                                        operator_stack[operator_stack_iterator++] = '@';
+
+                                operator_stack[operator_stack_iterator++] = character;
                         }
 
-                        if(character==')')
+                        else if (character == ')')
                         {
-                                while(operator_stack[operator_stack_iterator-1]!='(')
+                                while (operator_stack[operator_stack_iterator - 1] != '(')
                                 {
                                         operator_stack_iterator--;
                                         char operator_as_string[2];
-                                        operator_as_string[0]=operator_stack[operator_stack_iterator];
-                                        operator_as_string[1]='\0';
-                                        strcpy(rpn[rpn_iterator].expression,operator_as_string);
-                                        rpn[rpn_iterator].type=OPERATOR;
+                                        operator_as_string[0] = operator_stack[operator_stack_iterator];
+                                        operator_as_string[1] = '\0';
+                                        strcpy(rpn[rpn_iterator].expression, operator_as_string);
+                                        rpn[rpn_iterator].type = OPERATOR;
                                         rpn_iterator++;
                                 }
                                 operator_stack_iterator--;
-                                continue;
                         }
 
-                        if(operator_stack_iterator>0)
-                        {
-                                if(weight(character)>weight(operator_stack[operator_stack_iterator-1]))
-                                        operator_stack[operator_stack_iterator++]=character;
-                                else
-                                {
-                                        while(operator_stack_iterator>0 && weight(operator_stack[operator_stack_iterator-1])>=weight(character))
-                                        {
-                                                operator_stack_iterator--;
-                                                char operator_as_string[2];
-                                                operator_as_string[0]=operator_stack[operator_stack_iterator];
-                                                operator_as_string[1]='\0';
-                                                strcpy(rpn[rpn_iterator].expression,operator_as_string);
-                                                rpn[rpn_iterator].type=OPERATOR;
-                                                rpn_iterator++;
-                                        }
-                                        operator_stack[operator_stack_iterator++]=character;
-                                }
-                        }
                         else
-                                operator_stack[operator_stack_iterator++]=character;
-
+                        {
+                                while (operator_stack_iterator > 0 && operator_stack[operator_stack_iterator - 1] != '(' && (weight(operator_stack[operator_stack_iterator - 1]) > weight(character) || (weight(operator_stack[operator_stack_iterator - 1]) == weight(character) && associativity(operator_stack[operator_stack_iterator - 1]) == LEFT_ASSOCIATIVITY)))
+                                {
+                                        operator_stack_iterator--;
+                                        char operator_as_string[2];
+                                        operator_as_string[0] = operator_stack[operator_stack_iterator];
+                                        operator_as_string[1] = '\0';
+                                        strcpy(rpn[rpn_iterator].expression, operator_as_string);
+                                        rpn[rpn_iterator].type = OPERATOR;
+                                        rpn_iterator++;
+                                }
+                                operator_stack[operator_stack_iterator++] = character;
+                        }
                 }
                 else
-                        appendString(current_expression,character);
+                        appendString(current_expression, character);
 
+                expression_iterator++;
+        } while (expression_iterator < size_of_expression);
 
-        } while(expression_iterator<size_of_expression);
+        if (current_expression[0] != '\0')
+        {
+                strcpy(rpn[rpn_iterator].expression, current_expression);
+                rpn[rpn_iterator].type = EXPRESSION;
+                rpn_iterator++;
+                current_expression[0] = '\0';
+        }
 
-
+        while (operator_stack_iterator > 0)
+        {
+                operator_stack_iterator--;
+                char operator_as_string[2];
+                operator_as_string[0] = operator_stack[operator_stack_iterator];
+                operator_as_string[1] = '\0';
+                strcpy(rpn[rpn_iterator].expression, operator_as_string);
+                rpn[rpn_iterator].type = OPERATOR;
+                rpn_iterator++;
+        }
+        return rpn;
 }
--- tree.h
+++ tree.h
@@ -3,16 +3,17 @@
 
 #include "io.h"
 
-
 #define OPERATOR 1
 #define EXPRESSION 0
 #define RPN_ARRAY_SIZE 512
+#define RIGHT_ASSOCIATIVITY 1
+#define LEFT_ASSOCIATIVITY 0
 
 typedef struct unit
 {
         char expression[STRING_SIZE];
         bool type;
-}unit;
+} unit;
 
 typedef struct node
 {
@@ -21,10 +22,11 @@ typedef struct node
 } node;
 
 node *createNode(char *expression);
-node *operateOnNode(node *first,node *second, char operator);
+node *operateOnNode(node *first, node *second, char operator);
 
 bool isOperator(char character);
 int weight(char operator);
+int associativity(char operator);
 node *convertExpressionToTree(char *expression);
 unit *convertAlgebraicToRPN(char *expression);
 node *convertRPNToTree(unit *rpn);
